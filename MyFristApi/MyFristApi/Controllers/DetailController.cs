d2b9b7a06a923a8f8020f3c8444b0e2b3e635642
bobby1072 <75732572+bobby1072@users.noreply.github.com>
2022-06-29 15:34:30 +0100

    Add files via upload

---
+++ MyFristApi/MyFristApi/Controllers/DetailController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace MyFristApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DetailController : Controller
+    {
+        private readonly ILogger<DetailController> _logger;
+
+        public DetailController(ILogger<DetailController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("ShowDetails")]
+        
+        public List<ReadDetails> ShowDetails()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://dbuser:dbuser@cluster0.sssd1.mongodb.net/?retryWrites=true&w=majority");
+            var database = dbClient.GetDatabase("testdb");
+            var collection = database.GetCollection<BsonDocument>("peopleReg");
+            var documents = collection.Find(new BsonDocument()).ToList();
+            List<ReadDetails> jsonStringList = new List<ReadDetails>(); 
+            foreach (BsonDocument doc in documents)
+            {
+                ReadDetails recordObject = new ReadDetails(doc);
+                jsonStringList.Add(recordObject);
+            }
+            return jsonStringList;
+        }
+        [HttpPost("SubmitDetails")]
+        public string Post(string name, string dOB)
+        {
+            string result;
+            try
+            {
+                var DOB = DateTime.Parse(dOB);
+                var record = new SubmitDetails(name, DOB);
+                record.Submit();
+                result = "Submitted Correctly";
+            }
+            catch
+            {
+                result = "Error";
+            }
+            return result;
+        }
+    }
+}
---
+++ MyFristApi/MyFristApi/DetailsClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+namespace MyFristApi.Controllers
+{
+    public abstract class Details
+    {
+        public string Name;
+        public DateTime DOB;
+    }
+    public class ReadDetails : Details
+    {
+        public string Name { get; }
+        public DateTime DOB { get; }
+        public ReadDetails(BsonDocument bsonDoc)
+        {
+            this.Name = bsonDoc.GetValue("Name").ToString();
+            this.DOB = Convert.ToDateTime(bsonDoc.GetValue("DateOfBirth"));
+        }
+    }
+    public class SubmitDetails : Details
+    {
+        public string Name { get; }
+        public DateTime DOB { get; }
+        public BsonDocument record { get; }
+        public SubmitDetails(string Name, DateTime DOB)
+        {
+            this.Name = Name;
+            this.DOB = DOB;
+            this.record = new BsonDocument { { "Name", this.Name }, { "DateOfBirth", this.DOB } };
+        }
+        public void Submit()
+        {
+            MongoClient dbClient = new MongoClient("mongodb+srv://dbuser:dbuser@cluster0.sssd1.mongodb.net/?retryWrites=true&w=majority");
+            var database = dbClient.GetDatabase("testdb");
+            var collection = database.GetCollection<BsonDocument>("peopleReg");
+            collection.InsertOne(record);
+        }
+    }
+}
